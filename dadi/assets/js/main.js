e8fe9d2fac34866ef9ba380fbe36281fd860af8e
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-28 15:28:09 +0200

    completamento esercizio

--- dadi/assets/js/main.js
+++ dadi/assets/js/main.js
@@ -4,3 +4,17 @@ Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto.
 */
+
+function play(){
+    let person = Math.floor(Math.random()*6)+1
+
+    let pc = Math.floor(Math.random()*6)+1
+
+    if(person>pc){
+        document.getElementById(`result`).innerHTML = `<p>You: ${person} PC:  ${pc}, the winner are you</p>`
+    } else if(person<pc){
+        document.getElementById(`result`).innerHTML = `<p>You: ${person} PC:  ${pc}, the winner is pc</p>`
+    } else{
+        document.getElementById(`result`).innerHTML = `<p>You: ${person} PC:  ${pc}, no winners!</p>`
+    }
+}
--- email/assets/js/main.js
+++ email/assets/js/main.js
@@ -5,3 +5,32 @@ Chiedi all’utente la sua email,
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
 */
+
+function check(){
+    let checkEmail = document.getElementById(`checkEmail`).value;
+
+    let email = [
+        `mario.rossi@gmail.com`,
+        `mario.rossi2@gmail.com`,
+        `paolo.bianchi@gmail.com`,
+        `paolo.bianchi2@gmail.com`,
+        `roberto.verdi@gmail.com`,
+        `roberto.verdi2@gmail.com`,
+        `giorgio.neri@gmail.com`,
+        `giorgio.neri2@gmail.com`,
+    ]
+
+    let soldatino = false;
+
+    for(let n = 0; n < email.length; n++){
+        if(email[n]==checkEmail){
+            soldatino = true;
+        }
+    }
+
+    if(soldatino==true){
+        document.getElementById(`answer`).innerHTML = `<p>Accesso consentito</p>`
+    } else{
+        document.getElementById(`answer`).innerHTML = `<p>Accesso negato</p>`
+    }
+}
